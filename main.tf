fe3834558de07323f9464f222f7d25d35ec2a081
felix-murugan <felix.murugan@digidense.in>
2025-08-29 09:59:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_network" "vpc_network" {
+  name = "server-networks"
+}
+
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "web_ports" {
+  name    = "allow-web-traffic"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080","3000","3001","4000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["web-enabled"]
+}
