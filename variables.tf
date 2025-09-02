27831db99edc22fa5d30cbeb4eb3223e5ddb49bb
felix-murugan <felix.murugan@digidense.in>
2025-09-02 15:14:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,10 +11,10 @@ variable "region" {
 
 variable "zone" {
   default     = "us-central1-a"
-  description = "Zone to deploy intos"
+  description = "Zone to deploy into"
 }
 
 variable "instance_count" {
   description = "Number of VM instances to create"
-  default = 1
+  default     = 1
 }
