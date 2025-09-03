e8fb9d8ceeae22dfcbfdb5c900bcd6c358f249eb
felix-murugan <felix.murugan@digidense.in>
2025-09-03 11:27:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,15 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "red-global-467504"
+  default     = "swagger-470910"
 }
 
 variable "region" {
-  default     = "us-central1-a"
   description = "Region to deploy into"
+  default     = "us-central1-a"
 }
 
 variable "zone" {
-  default     = "us-central1-a"
   description = "Zone to deploy into"
-}
-
-variable "instance_count" {
-  description = "Number of VM instances to create"
-  default     = 1
+  default     = "us-central1-a"
 }
