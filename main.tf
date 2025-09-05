301b84dd07f54b87c4a1639ea719512bec56fc11
felix-murugan <felix.murugan@digidense.in>
2025-09-05 14:01:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,5 +54,13 @@ resource "google_compute_instance" "server_vm" {
     access_config {}
   }
 
+  metadata = {
+    enable-oslogin = "FALSE"
+    startup-script = file("${path.module}/deployment.sh")
+    ssh-keys       = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
+
+  }
   metadata_startup_script = file("${path.module}/deployment.sh")
+
+  tags = ["ssh-enabled", "web-enabled"]
 }
