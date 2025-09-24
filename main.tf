514bf92394e7f0de2c502b3a4d25d5a6bd6911d3
felix-murugan <felix.murugan@digidense.in>
2025-09-24 12:18:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# ===== VPC Network =====
 resource "google_compute_network" "vpc_network" {
   name = "server-networks"
 }
 
+# ===== Firewall Rules =====
 resource "google_compute_firewall" "ssh" {
   name    = "allow-ssh-2"
   network = google_compute_network.vpc_network.name
@@ -24,17 +26,19 @@ resource "google_compute_firewall" "web_ports" {
   }
 
   source_ranges = ["0.0.0.0/0"]
-
-  target_tags = ["web-enabled"]
+  target_tags   = ["web-enabled"]
 }
 
+# ===== Get current user email for SSH =====
 data "google_client_openid_userinfo" "me" {}
 
+# ===== SSH Key =====
 resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# ===== Compute Instance =====
 resource "google_compute_instance" "server_vm" {
   name         = "server-2"
   machine_type = "e2-medium"
@@ -50,23 +54,27 @@ resource "google_compute_instance" "server_vm" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network       = google_compute_network.vpc_network.name
     access_config {}
-  }
-
-
-  metadata = {
-    enable-oslogin = "FALSE"
-    startup-script = file("${path.module}/deployment.sh")
-    ssh-keys       = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
   }
 
   tags = ["ssh-enabled", "web-enabled"]
 
+  metadata = {
+    enable-oslogin = "FALSE"
+
+    # ===== Inject GitHub Token into startup script =====
+    startup-script = <<-EOT
+      #!/bin/bash
+      export GITHUB_TOKEN="${GITHUB_TOKEN}"
+      /bin/bash /deployment.sh
+    EOT
+
+    ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${tls_private_key.ssh.public_key_openssh}"
+  }
 }
 
-
+# ===== Outputs =====
 output "server_vm_ip" {
   description = "Public IP address of the server VM"
   value       = google_compute_instance.server_vm.network_interface[0].access_config[0].nat_ip
-}
